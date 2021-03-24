9a2c4be8320277bda9614f62f7c3ecaf136c073d
pedrosv <pedrosv6@gmail.com>
2021-03-24 19:29:53 -0300

    First Commit

---
+++ src/test/java/com/pedrosv/vacinacao/VacinacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrosv.vacinacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacinacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
